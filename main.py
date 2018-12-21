d71448476157885785f43c2cd9af0ea017269f2a
Petr <xflajs00@stud.fit.vutbr.cz>
2018-12-21 14:30:05 +0100

    add argument parser

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import string
+import sys
+import cv2
+import numpy
+import argparse
+
+parser = argparse.ArgumentParser(description='Panorama stitching.')
+parser.add_argument('--folder', help='path to file with images')
+parser.add_argument('--img', help='path to main image')
+parser.add_argument('--dest', help='destination of output image')
+
+
 
 def main():
     print("Hello world")
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    print(args)
     main()
