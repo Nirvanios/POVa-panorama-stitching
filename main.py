e5b46edb371bf3b4f97a0e156ccd131031ffa7c6
Petr <xflajs00@stud.fit.vutbr.cz>
2018-12-21 15:17:25 +0100

    implement sift key point detection

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ import cv2
 import numpy
 import argparse
 
+sift = cv2.xfeatures2d.SIFT_create()
+
 parser = argparse.ArgumentParser(description='Panorama stitching.')
 parser.add_argument('--folder', help='path to file with images')
 parser.add_argument('--img', help='path to main image')
 parser.add_argument('--dest', help='destination of output image')
 
 
+def get_sift(image):
+    """
+    Get key points detected by SIFT
+    """
+    gray_scale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    key_points = sift.detect(gray_scale_image, None)
+    return key_points
+
 
 def main():
     print("Hello world")
     print("dzea")
 
+
 if __name__ == "__main__":
     args = parser.parse_args()
-    print(args)
     main()
