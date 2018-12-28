c82ac95e11e18672ee73a62df1a2c4b9d567795f
Petr <xflajs00@stud.fit.vutbr.cz>
2018-12-28 10:50:33 +0100

    refactoring, add cylinder projection to homography panorama

--- PanoramaStitching/Stitcher.py
+++ PanoramaStitching/Stitcher.py
@@ -52,6 +52,7 @@ def stitch_images_affine(img1, img2, M):
     :param M: affine matrix
     :return: stitched image
     """
+
     out1 = cv2.warpAffine(img2, M, (img1.shape[1], img1.shape[0]))
 
     output = np.zeros(img1.shape)
@@ -68,6 +69,39 @@ def stitch_images_affine(img1, img2, M):
                 output[i][j] = (img1[i][j])
 
     return output.astype(np.uint8)
+
+    rows1, cols1 = img1.shape[:2]
+    rows2, cols2 = img2.shape[:2]
+
+    list_of_points_1 = np.float32([[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+    temp_points = np.float32([[0, 0], [0, rows2], [cols2, rows2], [cols2, 0]]).reshape(-1, 1, 2)
+
+    list_of_points_2 = cv2.transform(temp_points, M)
+    list_of_points = np.concatenate((list_of_points_1, list_of_points_2), axis=0)
+
+    [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
+    [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
+
+    translation_dist = [-x_min, -y_min]
+
+    output_img = cv2.warpAffine(img2, M, (x_max - x_min, y_max - y_min))
+
+    temp_image = np.zeros((y_max - y_min, x_max - x_min, 3), np.uint8)
+
+    # Blender.alpha_blend(img1, output_img, translation_dist)
+
+    temp_image[translation_dist[1]:rows1 + translation_dist[1],
+    translation_dist[0]:cols1 + translation_dist[0]] = img1
+
+    width, height, _ = temp_image.shape
+
+    for x in range(width):
+        for y in range(height):
+            maxpix = temp_image[x, y].max()
+            if output_img[x, y, 0] > maxpix or output_img[x, y, 1] > maxpix or output_img[x, y, 2] > maxpix:
+                temp_image[x, y] = output_img[x, y]
+
+    return temp_image
 
 
 def stitch_images_homography(img1, img2, homography_matrix):
